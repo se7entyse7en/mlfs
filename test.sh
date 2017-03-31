0cf1631ca2b8ab78db66515bad007ba28612b18c
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2017-04-01 01:47:52 +0200

    Added test for k-fold cross validation applied to KNN

--- test.sh
+++ test.sh
@@ -4,8 +4,10 @@ if [[ "$1" == "static_checks" ]]; then
     isort -rc -c -q
 elif [[ "$1" == "models_demo" ]]; then
     echo "Running models..."
-    echo "Running K-Nearest Neighbors with k=3 and distance=L1.."
-    python -m mlfs.supervised.knn run --k 3 --distance L1
-    echo "Running K-Nearest Neighbors with k=3 and distance=L2.."
-    python -m mlfs.supervised.knn run --k 3 --distance L2
+    echo "Running K-Nearest Neighbors with k=3 and distance=L1..."
+    python -m mlfs.supervised.knn run_simple --k 3 --distance L1
+    echo "Running K-Nearest Neighbors with k=3 and distance=L2..."
+    python -m mlfs.supervised.knn run_simple --k 3 --distance L2
+    echo "Running K-Fold Cross validation for K-Nearest Neighbors with k=3, distance=L2 and 5 folds..."
+    python -m mlfs.supervised.knn run_k_fold --k 3 --distance L2 --folds_n 5
 fi
